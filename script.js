99085d8e10755e8797ccff42badbc866eb900d2b
SAURAV Kumar <saurav12112000@gmail.com>
2023-08-22 11:29:42 +0530

    addition

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let string = "";
+const input = document.getElementById("input");
+const output = document.getElementById("result");
+
 function calc() {
   string = eval(string);
   document.querySelector("input").value = string;
@@ -14,28 +17,72 @@ function updateResult() {
 let buttons = document.querySelectorAll(".button");
 Array.from(buttons).forEach((button) => {
   button.addEventListener("click", (e) => {
+    console.log(e.target.innerHTML);
     if (e.target.innerHTML == "=") {
       calc();
     } else if (e.target.innerHTML == "C") {
       string = "";
       document.querySelector("input").value = string;
-    } else {
+    }
+    else if(e.target.innerHTML=="%"){
+      string = string/100;
+      output.textContent = string;
+    }
+    
+    else {
       console.log(e.target);
-      string = string + e.target.innerHTML;
-      document.querySelector("input").value = string;
+      console.log(e.target.innerHTML);
+      if(string.length>1){
+      let string1 = string.slice(-1);
+      console.log(string1);
+      if(string1===e.target.innerHTML && (string1==="*"|| string1==="/"||string1==="+"|| string1==="-" || string1===".") )
+      {
+      // if(string1===e.target.innerHTML){
+        
+        string = string;
+      }
+
+      else{
+        string = string + e.target.innerHTML;
+        console.log(string);
+        document.querySelector("input").value = string;
+        console.log(document.querySelector("input").value);
+      }
+    }
+
+      else{
+        string = string + e.target.innerHTML;
+        console.log(string);
+        document.querySelector("input").value = string;
+        console.log(document.querySelector("input").value);
+      }
+    
+      // string = string + e.target.innerHTML;
+      // console.log(string);
+      // document.querySelector("input").value = string;
+      // console.log(document.querySelector("input").value);
+      
       updateResult();
     }
   });
 });
 
-const input = document.getElementById("input");
-const output = document.getElementById("result");
 
 
-function appendToInput(value){
-  input.value+=value;
+// function appendToInput(value){
+  
+//   // input.value+=value;
+//   console.log(value);
+//   console.log(input.value.includes(value));
+//   if(input.value.includes(value)){
+//     input.value;
+//   }
+//   // else{
+//   //   input.value+=value;
+//   // }
+//   console.log(input.value);
 
-}
+// }
 
 function clearInput(){
   input.value = "";
@@ -44,11 +91,19 @@ function clearInput(){
 
 
 function updateResult() {
-  try {
+  // try {
     const inputField = input.value;
+    console.log(inputField);
+    if(inputField.includes( "/0")){
+      string = "Cannot be divided by zero";
+      output.textContent = string;
+    }
+    else{
     const result = eval(inputField);
     output.textContent = result;
-  } catch (err) {
-   output.textContent='';
-  }
+    }
+  // } 
+  // catch (err) {
+  //  output.textContent= '';
+  // }
 }
